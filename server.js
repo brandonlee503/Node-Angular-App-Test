c15c5c7d4d0dd144640781fd08cb3576f89e176f
Brandon <leebran@onid.oregonstate.edu>
2015-08-05 20:09:29 -0700

    Created initial server

--- server.js
+++ server.js
@@ -6,8 +6,31 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var bodyParser = require('body-parser');          // Pull info from HTML POST
+var methodOverride = require('method-override');  // Simulate DELETE and PUT
 
+// =============================================================================
 // Configuration
+
+// DB Config
 const DB_URL = argv.db || process.env.DATABASE_URL;
+mongoose.connect(DB_URL);
+
+// Set static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public');
+
+// Log every request to the console
+app.use(morgan('dev'));
+
+// Parse application/x-www-form-unlencoded, JSON, and vnd.api+json as JSON
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.use(methodOverride());
+
+
+// =============================================================================
+// Initilize app
+app.listen(8080);
+console.log("App is running on port 8080");
