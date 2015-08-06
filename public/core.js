41174bfa88801da27defbbec0a4844c78fc9dc11
Brandon <leebran@onid.oregonstate.edu>
2015-08-06 16:42:59 -0700

    Started creating Angular module and controller

---
+++ public/core.js
@@ -0,0 +1,20 @@
+// core.js
+
+'use strict';
+
+// Declare module
+const brandonTodo = angular.module('brandonTodo', []);
+
+function mainController($scope, $http) {
+  $scope.formdata = {};
+
+  // When landing on the page, get all todos and display them
+  $http.get('/api/todo')
+    .success(function(data) {
+      $scope.todos = data;
+      console.log(data);
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+}
